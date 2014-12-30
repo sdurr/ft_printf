e0baa9f71e44de207d78fc50be511cba6f6c5527
getrembl <getrembl@student.42.fr>
2014-12-30 17:25:41 +0100

    add negative to octal in function ft_print_o.c

--- ft_print_o.c
+++ ft_print_o.c
@@ -6,28 +6,57 @@
 /*   By: getrembl <getrembl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/29 22:44:50 by getrembl          #+#    #+#             */
-/*   Updated: 2014/12/30 14:03:02 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/12/30 17:25:14 by getrembl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "libft.h"
 
-int			ft_print_o(va_list ap)
+static int			ft_print_o_negative(int decimal)
 {
-	char	*octal;
-	int		quotient;
-	int		rest;
-	int		i;
-	char	*ret;
+	unsigned int	quotient;
+	int				rest;
+	int				i;
+	char			*octal;
+	char			*ret;
 
-	octal = ft_strnew(33);
-	quotient = va_arg(ap, int);
+	octal = ft_strnew(12);
+	quotient = 4294967296 + decimal;
 	i = 0;
 	while (quotient != 0)
 	{
 		rest = quotient % 8;
-		(quotient > 7) ? (quotient = quotient / 8) : (quotient = 0);
+		(quotient > 7) ? (quotient /= 8) : (quotient = 0);
+		rest = rest + 48;
+		octal[i++] = rest;
+	}
+	octal[i--] = '\0';
+	ret = ft_strnew(ft_strlen(octal) + 1);
+	rest = 0;
+	while (i >=0)
+		ret[rest++] = octal[i--];
+	ft_putstr(ret);
+	return (ft_strlen(octal));
+}
+
+int					ft_print_o(va_list ap)
+{
+	char			*octal;
+	int				quotient;
+	int				rest;
+	int				i;
+	char			*ret;
+
+	quotient = va_arg(ap, int);
+	if (quotient < 0)
+		return (ft_print_o_negative(quotient));
+	octal = ft_strnew(12);
+	i = 0;
+	while (quotient != 0)
+	{
+		rest = quotient % 8;
+		(quotient > 7) ? (quotient /= 8) : (quotient = 0);
 		rest = rest + 48;
 		octal[i++] = rest;
 	}
