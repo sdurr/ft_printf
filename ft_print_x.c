559cbf1ec73bb714919ee4e36df9bfe5f0dda741
getrembl <getrembl@student.42.fr>
2014-12-30 17:18:48 +0100

    add negative to hexadecimal in function ft_print_x_maj.c

--- ft_print_x.c
+++ ft_print_x.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/28 12:10:23 by sdurr             #+#    #+#             */
-/*   Updated: 2014/12/30 17:08:51 by getrembl         ###   ########.fr       */
+/*   Updated: 2014/12/30 17:11:03 by getrembl         ###   ########.fr       */
 /*   Updated: 2014/12/29 22:47:45 by getrembl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -14,7 +14,7 @@
 #include "libft.h"
 #include <stdarg.h>
 
-static int	ft_print_x_negative(int decimal)
+static int			ft_print_x_negative(int decimal)
 {
 	unsigned int	quotient;
 	int				rest;
@@ -41,13 +41,13 @@ static int	ft_print_x_negative(int decimal)
 	return (ft_strlen(hexa));
 }
 
-int			ft_print_x(va_list ap)
+int					ft_print_x(va_list ap)
 {
-	char	*hexa;
-	int		quotient;
-	int		rest;
-	int		i;
-	char	*ret;
+	char			*hexa;
+	int				quotient;
+	int				rest;
+	int				i;
+	char			*ret;
 
 	quotient = va_arg(ap, int);
 	if (quotient < 0)
--- ft_print_x_maj.c
+++ ft_print_x_maj.c
@@ -6,12 +6,39 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/28 12:10:23 by sdurr             #+#    #+#             */
-/*   Updated: 2014/12/29 19:05:38 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/12/30 17:17:57 by getrembl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdarg.h>
+
+static int			ft_print_x_maj_negative(int decimal)
+{
+	unsigned int	quotient;
+	int		rest;
+	int		i;
+	char	*ret;
+	char	*hexa;
+
+	quotient = 4294967296 + decimal;
+	hexa = ft_strnew(9);
+	i = 0;
+	while (quotient != 0)
+	{
+		rest = quotient % 16;
+		(quotient > 15) ? (quotient /= 16) : (quotient = 0);
+		(rest < 10) ? (rest = 48 + rest) : (rest = 55 + rest);
+		hexa[i++] = rest;
+	}
+	hexa[i--] = '\0';
+	ret = ft_strnew(ft_strlen(hexa) + 1);
+	rest = 0;
+	while (i >= 0)
+		ret[rest++] = hexa[i--];
+	ft_putstr(ret);
+	return (ft_strnew(hexa));
+}
 
 int		ft_print_x_maj(va_list ap)
 {
@@ -21,13 +48,15 @@ int		ft_print_x_maj(va_list ap)
 	int		i;
 	char	*ret;
 
-	hexa = ft_strnew(100);
 	quotient = va_arg(ap, int);
+	if (quotient < 0)
+		return (ft_print_x_maj_negative(quotient));
+	hexa = ft_strnew(100);
 	i = 0;
 	while (quotient != 0)
 	{
 		rest = quotient % 16;
-		(quotient > 15) ? (quotient = quotient / 16) : (quotient = 0);
+		(quotient > 15) ? (quotient /= 16) : (quotient = 0);
 		(rest < 10) ? (rest = 48 + rest) : (rest = 55 + rest);
 		hexa[i++] = rest;
 	}
