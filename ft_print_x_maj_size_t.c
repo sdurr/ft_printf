855e703291e93d7d08056beb87e0a2760fa34798
sdurr <sdurr@e3r12p15.42.fr>
2015-01-08 14:28:39 +0100

    all

---
+++ ft_print_x_maj_size_t.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_x.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/28 12:10:23 by sdurr             #+#    #+#             */
+/*   Updated: 2015/01/08 10:45:51 by sdurr            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdarg.h>
+
+static int ft_point_space(char *s1, char *s, int i, char **aff, size_t stop)
+{
+	size_t j;
+	char *tmp;
+
+	tmp = ft_strnew(13);
+	j = 0;
+	if (s[i] == '.')
+	{
+		i--;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			tmp[j] = s[i];
+			i--;
+			j++;
+		}
+		tmp = ft_revers(tmp);
+		j = ft_atoi(tmp);
+		if (j > stop)
+			while (j > (ft_strlen(s1)))
+			{
+				*aff = ft_strjoin(*aff, " ");
+				j--;
+				if (j == stop)
+					return (0);
+			}
+	}
+	return (0);
+}
+
+static int ft_number_befor(char *s1, char *s, int i, char **aff)
+{
+	char *tmp;
+	size_t j;
+
+	j = 0;
+	tmp = ft_strnew(13);
+	i--;
+	while (s[i] > '0' && s[i] <= '9')
+		tmp[j++] = s[i--];
+	tmp = ft_revers(tmp);
+	j = ft_atoi(tmp);
+	if ((s[i] == '.' && s[i + 1] == '0' && s[i - 1] == '%'))
+		return (0);
+	ft_point_space(s1, s, i, aff, j);
+	while (j > ft_strlen(s1))
+	{
+		if (s[i] == '.' || s[i + 1] == '0')
+			*aff = ft_strjoin (*aff, "0");
+		else
+			*aff = ft_strjoin(*aff, " ");
+		j--;
+	}
+		return (0);
+}
+
+static int			ft_print_x_maj_negative(int decimal, char *s, int j, char **aff)
+{
+	unsigned int	quotient;
+	int		rest;
+	int		i;
+	char	*ret;
+	char	*hexa;
+
+	quotient = 4294967296 + decimal;
+	hexa = ft_strnew(9);
+	i = 0;
+	while (quotient != 0)
+	{
+		rest = quotient % 16;
+		(quotient > 15) ? (quotient /= 16) : (quotient = 0);
+		(rest < 10) ? (rest += 48) : (rest += 55);
+		hexa[i++] = rest;
+	}
+	hexa[i--] = '\0';
+	ret = ft_strnew(ft_strlen(hexa) + 1);
+	rest = 0;
+	while (i >= 0)
+		ret[rest++] = hexa[i--];
+	quotient = ft_number_befor(hexa, s, j, aff);
+	if (s[j - 1] == '#' && ret[0] != '0')
+		*aff = ft_strjoin(*aff, "0X");
+		*aff = ft_strjoin(*aff, ret);
+	return (0);
+}
+
+
+int		ft_print_x_maj(va_list ap, char *s, int j, char **aff)
+{
+	char	*hexa;
+	int		quotient;
+	int		rest;
+	int		i;
+	char	*ret;
+
+	quotient = va_arg(ap, int);
+	if (quotient < 0)
+		return (ft_print_x_maj_negative(quotient, s, j, aff));
+	if ((quotient == 0 && s[j - 1] == '.') || (quotient == 0 && s[j - 1] == '0'))
+		return (0);
+	if (quotient == 0)
+	{
+		*aff = ft_strjoin(*aff, "0");
+		return (0);
+	}
+	hexa = ft_strnew(15);
+	i = 0;
+	while (quotient != 0)
+	{
+		rest = quotient % 16;
+		(quotient > 15) ? (quotient /= 16) : (quotient = 0);
+		(rest < 10) ? (rest += 48) : (rest += 55);
+		hexa[i++] = rest;
+	}
+	hexa[i--] = '\0';
+	ret = ft_strnew(ft_strlen(hexa) + 1);
+	rest = 0;
+	while (i >= 0)
+		ret[rest++] = hexa[i--];
+	quotient = ft_number_befor(hexa, s, j, aff);
+	if (s[j - 1] == '#' && ret[0] != '0')
+		*aff = ft_strjoin(*aff, "0X");
+	*aff = ft_strjoin(*aff, ret);
+	return (0);
+}
+
--- ft_type.c
+++ ft_type.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/27 15:24:57 by sdurr             #+#    #+#             */
-/*   Updated: 2015/01/08 12:26:54 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/01/08 14:24:57 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	ft_type(char *s, int i, va_list ap, char **aff)
 	}
 	if (s[i -1] == 'l' && s[i] == 'd' && s[j + 2] == 'l')
 		return (ft_print_d_long_long(ap, s, i, aff));
+	if (s[i - 1] == 'z' && s[i] == 'x')
+		return(ft_print_x_size_t(ap, s, i, aff));
 	if (s[i - 1] == 'z')
 		return(ft_print_size_t(ap, s, i, aff));
 	if ((s[i] == 'D') || (s[i] == 'd' && s[j + 1] == 'l') || (s[i] == 'i' && s[j + 1] == 'l') || (s[i] == 'd' && s[j + 1] == 'j') || (s[i] == 'i' && s[j + 1] == 'j'))
