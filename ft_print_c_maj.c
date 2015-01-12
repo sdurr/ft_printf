f9bdb79f1bfa2bcfefd8c84a80ba8d86c8d954da
getrembl <getrembl@student.42.fr>
2015-01-12 19:09:02 +0100

    mal au crane avec le wchar_t je vais manger

--- ft_print_c_maj.c
+++ ft_print_c_maj.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/27 15:23:12 by sdurr             #+#    #+#             */
-/*   Updated: 2015/01/12 17:41:02 by getrembl         ###   ########.fr       */
+/*   Updated: 2015/01/12 18:05:20 by getrembl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,27 @@
 #include <stdarg.h>
 #include <wchar.h>
 #include "libftprintf.h"
+/*
+static int		ft_dectobin(wchar_t c)
+{
+	return (0);
+}
 
+static wchar_t	*itow(unsigned long val)
+{
+	static wchar_t buf[30];
+	wchar_t *wcp = &buf[29];
+	*wcp = L'\0';
+	while (val != 0)
+	{
+		*--wcp = btowc ('0' + val % 10);
+		val /= 10;
+	}
+	if (wcp == &buf[29])
+		*--wcp = L'0';
+	return wcp;
+}
+*/
 int				ft_print_c_maj(va_list ap, char *s, int i)
 {
 	wchar_t		c;
@@ -28,12 +48,12 @@ int				ft_print_c_maj(va_list ap, char *s, int i)
 		tmp[j++] = s[i--];
 	tmp = ft_revers(tmp);
 	j = ft_atoi(tmp);
-	c = va_arg(ap, wchar_t);
+	c = (wchar_t)va_arg(ap, unsigned int);
 	while (j-- > 1)
 		ft_putchar (' ');
-	if (c > 0x0000 && c < 0x007F)
+	if (c >= 0x0000 && c <= 0x007F)
 		ft_putchar((char)c);
-	else if (c > 0x0080 && c < 0x1F8FF)
+	if (c >= 0x0080 && c <= 0x1F8FF)
 		ft_putwchar(c);
 	else
 		return (1);
