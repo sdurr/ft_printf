20752f4d8685596a70aa960717972a2f345628af
sdurr <sdurr@e3r1p12.42.fr>
2015-02-02 12:42:56 +0100

    strnew 2 printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/27 15:23:57 by sdurr             #+#    #+#             */
-/*   Updated: 2015/02/02 08:57:38 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/02/02 12:42:01 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ static int		test_i(char *s, int i, int test, char **s1)
 
 static int		ft_printf_suite(va_list ap, int *i, char *s[4])
 {
-	while (s[0][++i[0]] && (s[1] = ft_strnew(1)))
+	while (s[0][++i[0]] && (s[1] = ft_strnew(2)))
 	{
 		while (s[0][i[0]] != '%' && s[0][i[0]] && (s[2][0] = s[0][i[0]++]))
 			s[1] = ft_strjoin(s[1], s[2]);
